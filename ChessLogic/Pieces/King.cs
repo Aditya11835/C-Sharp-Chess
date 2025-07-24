034fba6326a29347f6f7ce7b35230efb9ebb35f9
Aditya11835 <imapoorvanegi@gmail.com>
2025-07-24 18:06:53 +0530

    Add normal move logic for King Knight and Pawn

--- ChessLogic/Pieces/King.cs
+++ ChessLogic/Pieces/King.cs
@@ -1,9 +1,22 @@
-﻿namespace ChessLogic
+﻿using System.Runtime.Serialization;
+using ChessLogic.Moves;
+namespace ChessLogic
 {
     public class King : Piece
     {
         public override PieceType Type => PieceType.King;
         public override Player Color { get; }
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.South,
+            Direction.West,
+            Direction.East,
+            Direction.NorthEast,
+            Direction.SouthEast,
+            Direction.NorthWest,
+            Direction.SouthWest
+        };
         public King(Player color)
         {
             Color = color;
@@ -13,6 +26,21 @@
             King copy = new King(Color);
             copy.hasMoved = hasMoved;
             return copy;
+        }
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            foreach (Direction dir in dirs)
+            {
+                Position to = from + dir;
+                if (!Board.IsInside(to))
+                {
+                    continue;
+                }
+                if (board.IsEmpty(to) || board[to].Color != Color)
+                {
+                    yield return to;
+                }
+            }
         }
     }
 }
