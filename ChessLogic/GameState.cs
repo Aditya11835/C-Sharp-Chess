bcc835a0e3319cedb0b87d4d89a44b2666c14eb2
Aditya11835 <imapoorvanegi@gmail.com>
2025-08-03 16:12:00 +0530

    Add Threefold Repetition

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -8,10 +8,15 @@ namespace ChessLogic
         public Player CurrentPlayer { get; private set; }
         public Result Result { get; private set; } = null;
         private int noCaptureOrPawnMoves = 0;
+        private string stateString;
+        private readonly Dictionary<string, int> stateHistory = new Dictionary<string, int>();
         public GameState(Player player, Board board)
         {
             CurrentPlayer = player;
             Board = board;
+
+            stateString = new StateString(CurrentPlayer, Board).ToString();
+            stateHistory[stateString] = 1;
         }
         public IEnumerable<Move> LegalMovesForPiece(Position pos)
         {
@@ -31,12 +36,14 @@ namespace ChessLogic
             if (captureOrPawn)
             {
                 noCaptureOrPawnMoves = 0;
+                stateHistory.Clear();
             }
             else
             {
                 noCaptureOrPawnMoves++;
             }
-                CurrentPlayer = CurrentPlayer.Opponent();
+            CurrentPlayer = CurrentPlayer.Opponent();
+            UpdateStateString();
             CheckForGameOver();
         }
 
@@ -70,6 +77,10 @@ namespace ChessLogic
             {
                 Result = Result.Draw(EndReason.FiftyMoveRule);
             }
+            else if (ThreefoldRepetition())
+            {
+                Result = Result.Draw(EndReason.ThreefoldRepetition);
+            }
         }
         public bool IsGameOver()
         {
@@ -80,6 +91,23 @@ namespace ChessLogic
         {
             int fullMoves = noCaptureOrPawnMoves / 2;
             return fullMoves == 50;
+        }
+
+        private void UpdateStateString()
+        {
+            stateString = new StateString(CurrentPlayer, Board).ToString();
+            if (!stateHistory.ContainsKey(stateString))
+            {
+                stateHistory[stateString] = 1;
+            }
+            else
+            {
+                stateHistory[stateString]++;
+            }
+        }
+        private bool ThreefoldRepetition()
+        {
+            return stateHistory[stateString] == 3;
         }
     }
 }
